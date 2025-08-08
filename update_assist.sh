ec3498983b74ef7ea585d05e6a5b71d99fccbf4f
aaron <caaronh85@gmail.com>
2025-08-08 02:25:15 -0400

    added some test echo commands for future code

--- update_assist.sh
+++ update_assist.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-dnf history | grep -i "update" | awk '{print $5}' | head -2 
+last_update=$(dnf history | grep -i "update" | awk '{print $5}' | head -2 | grep -v "repositories") 
+current_ts=$(date +%s)
+last_update_ts=$(date -d "$last_update" +%s)
+
+days_diff=$(( (current_ts - last_update_ts) / 86400 ))
+
+if (( days_diff > 4 )); then
+	echo "please update"
+else
+	echo "test goodbye"
+	exit 1
+fi
+
